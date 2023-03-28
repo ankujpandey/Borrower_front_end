c9e08e272217fcaf3ef0892020c778614b50b3c0
ankujpandey <pandeyankuj@gmail.com>
2023-03-28 19:02:42 +0530

    Added Designs template and heroImage and NavBar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
 import EmploymentDetails from "./pages/EmploymentDetails";
-import AadharUploadComponent from "./components/AadharUploadComponent";
-import SignUpComponent from "./pages/SignUpPage";
-import LoginComponent from "./pages/LoginPage";
-import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom"; //npm i react-router-dom
 import Register from "./pages/Register";
 import Register3 from "./pages/Register3";
 import Register4 from "./pages/Register4";
 import LoginPage from "./pages/LoginPage";
-import NavBar from "./components/Navbar";
 import SignUpPage from "./pages/SignUpPage";
 import Dashboard from "./pages/Dashboard";
+import NavBar from "./components/NavBar";
+import HeroImage from "./components/HeroImage";
 
 function App() {
 	return (
@@ -25,14 +21,14 @@ function App() {
 			<NavBar />
 
 			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/dashboard" element={<Dashboard />} />
-				<Route path="/Sign-up" element={<SignUpPage />} />
-				<Route path="/Sign-in" element={<LoginPage />} />
-				<Route path="/register" element={<Register />} />
+				<Route path="/" element={<HeroImage />} />
+				{/* <Route path="/dashboard" element={<Dashboard />} /> */}
+				<Route path="/sign-up" element={<SignUpPage />} />
+				<Route path="/sign-in" element={<LoginPage />} />
+				{/* <Route path="/register" element={<Register />} />
 				<Route path="/register2" element={<EmploymentDetails />} />
 				<Route path="/register3" element={<Register3 />} />
-				<Route path="/register4" element={<Register4 />} />
+				<Route path="/register4" element={<Register4 />} /> */}
 			</Routes>
 		</div>
 	);
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/js/bootstrap.js";
+// import "bootstrap/dist/css/bootstrap.css";
+// import "bootstrap/dist/js/bootstrap.js";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
