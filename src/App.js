65d3290e5dbd892c0fbc670490072a8e1005f4df
ankujpandey <pandeyankuj@gmail.com>
2023-03-30 11:25:00 +0530

    merged all code till now

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import "./App.css";
 import EmploymentDetails from "./pages/EmploymentDetails";
-<<<<<<< HEAD
-=======
 // import AadharUploadComponent from "./components/AadharUploadComponent";
 // import SignUpComponent from "./pages/SignUpPage";
 // import LoginComponent from "./pages/LoginPage";
 import Home from "./pages/Home";
->>>>>>> 035746a68596ddfaaead62b34eb07a273077a50d
 import { Routes, Route } from "react-router-dom"; //npm i react-router-dom
 import Register from "./pages/Register";
 import Register3 from "./pages/Register3";
@@ -17,36 +14,32 @@ import Dashboard from "./pages/Dashboard";
 import NavBar from "./components/NavBar";
 import Address from "./components/Address";
 import BorrowingDetails from "./components/BorrowingDetails";
-<<<<<<< HEAD
-import Home from "./pages/Home";
-=======
 import { UserProvider } from "./components/UserContext";
->>>>>>> 035746a68596ddfaaead62b34eb07a273077a50d
 
 function App() {
-  return (
-    <UserProvider>
-      {/* <EmploymentDetails /> */}
-      {/* <AadharUploadComponent /> */}
-      {/* <SignUpComponent /> */}
-      {/* <LoginComponent /> */}
-      {/* <LoginPage /> */}
-      <NavBar />
+	return (
+		<UserProvider>
+			{/* <EmploymentDetails /> */}
+			{/* <AadharUploadComponent /> */}
+			{/* <SignUpComponent /> */}
+			{/* <LoginComponent /> */}
+			{/* <LoginPage /> */}
+			<NavBar />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/sign-up" element={<SignUpPage />} />
-        <Route path="/sign-in" element={<LoginPage />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/register2" element={<EmploymentDetails />} />
-        <Route path="/register3" element={<Register3 />} />
-        <Route path="/register4" element={<Register4 />} />
-        <Route path="/address" element={<Address />} />
-        <Route path="/borrowing-details" element={<BorrowingDetails />} />
-      </Routes>
-    </UserProvider>
-  );
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/dashboard" element={<Dashboard />} />
+				<Route path="/sign-up" element={<SignUpPage />} />
+				<Route path="/sign-in" element={<LoginPage />} />
+				<Route path="/register" element={<Register />} />
+				<Route path="/register2" element={<EmploymentDetails />} />
+				<Route path="/register3" element={<Register3 />} />
+				<Route path="/register4" element={<Register4 />} />
+				<Route path="/address" element={<Address />} />
+				<Route path="/borrowing-details" element={<BorrowingDetails />} />
+			</Routes>
+		</UserProvider>
+	);
 }
 
 export default App;
