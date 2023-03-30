ef414c8baffc978e98809f8e7c85ecba8fbd881e
Arzoo Jangra <aaa.jangra123@gmail.com>
2023-03-30 14:44:23 +0530

    Personalized Dashboard using context Api

--- src/hooks/useHandleValidation.jsx
+++ src/hooks/useHandleValidation.jsx
@@ -1,68 +1,46 @@
-import axios from "axios";
 import { useFormik } from "formik";
+import axios from "axios";
 import { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router";
-import { Axios } from "../functions/Axios";
+// import { Axios } from "../functions/Axios";
 import { UserContext } from "../components/UserContext";
 
 export const useHandleValidation = (
-	initialValues,
-	validationSchema,
-	url,
-	api
+  initialValues,
+  validationSchema,
+  url,
+  api
 ) => {
-	const navigate = useNavigate();
-	// const [flag, setFlag] = useState(false);
+  const { user, setUser } = useContext(UserContext);
+  const navigate = useNavigate();
 
-	const config = {
-		method: "post",
-		url: api,
-		headers: { "Content-Type": "application/json" },
-	};
+  const { values, errors, touched, handleBlur, handleChange, handleSubmit } =
+    useFormik({
+      initialValues,
+      validationSchema,
+      onSubmit: async (values, action) => {
+        const config = {
+          method: "post",
+          url: api,
+          data: values,
+          headers: { "Content-Type": "application/json" },
+        };
+        // console.log(values);
+        // const { loading, error, data } = Axios(config);
+        const res = await axios(config);
+        console.log(res?.data?.data);
 
-	const { values, errors, touched, handleBlur, handleChange, handleSubmit } =
-		useFormik({
-			initialValues,
-			validationSchema,
-			// onSubmit: (values) => {
-			//   // console.log("scsdnsdfds");
-			//   console.log(values);
-			//   // console.log(employeeType);
-			//   // setFlag(true);
-			// },
+        if (res.status === 201) {
+          setUser(res?.data?.data);
+          // console.log("user--", user);
+          localStorage.setItem("localUser", JSON.stringify(res?.data?.data));
+          navigate(url);
+        } else {
+          alert("Something went wrong!!!");
+        }
+        action.resetForm();
+      },
+    });
 
-			onSubmit: async (values, action) => {
-				console.log(values);
-				// try {
-				// 	const res = await axios.post(
-				// 		api,
-
-				// 		values,
-
-				// 		{
-				// 			headers: {
-				// 				"Content-Type": "application/json",
-				// 			},
-				// 		}
-				// 	);
-				// 	console.log(res);
-
-				const { loading, error, data } = Axios(config);
-
-				if (data.status === 201) {
-					// setFlag(true);
-					console.log(data.data);
-					localStorage.setItem("localUser", JSON.stringify(data?.data?.data));
-					navigate(url);
-				} else {
-					alert("Something went wrong!!!");
-				}
-				// } catch (error) {
-				// 	console.log(error);
-				// }
-				action.resetForm();
-			},
-		});
-
-	return { values, errors, touched, handleBlur, handleChange, handleSubmit };
+  return { values, errors, touched, handleBlur, handleChange, handleSubmit };
 };
