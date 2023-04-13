4133d74c2e10753bf6c4845277b0b75571a404ad
Arzoo Jangra <aaa.jangra123@gmail.com>
2023-04-13 15:58:58 +0530

    updated image css

--- src/components/CaptureImage.jsx
+++ src/components/CaptureImage.jsx
@@ -9,190 +9,194 @@ import { Camera } from "@mediapipe/camera_utils"; //npm i @mediapipe/camera_util
 import AadharUploadComponent from "./AadharUploadComponent";
 
 function CaptureImage(props) {
-	const [image, setImage] = useState(null);
-	const [seconds, setSeconds] = useState(0);
-	const { id } = useParams();
-	const [flag, setFlag] = useState(false);
-	const [warning, setWarning] = useState(false);
-	const navigate = useNavigate();
+  const [image, setImage] = useState(null);
+  const [seconds, setSeconds] = useState(0);
+  const { id } = useParams();
+  const [flag, setFlag] = useState(false);
+  const [warning, setWarning] = useState(false);
+  const navigate = useNavigate();
 
-	let picture = null;
+  let picture = null;
 
-	//Face Detection------->>>>
-	const { webcamRef, isLoading, boundingBox, facesDetected } = useFaceDetection(
-		{
-			faceDetectionOptions: {
-				model: "short",
-			},
-			faceDetection: new FaceDetection.FaceDetection({
-				locateFile: (file) =>
-					`https://cdn.jsdelivr.net/npm/@mediapipe/face_detection/${file}`,
-			}),
-			camera: ({ mediaSrc, onFrame, width, height }) =>
-				new Camera(mediaSrc, {
-					onFrame,
-					width,
-					height,
-				}),
-		}
-	);
+  //Face Detection------->>>>
+  const { webcamRef, isLoading, boundingBox, facesDetected } = useFaceDetection(
+    {
+      faceDetectionOptions: {
+        model: "short",
+      },
+      faceDetection: new FaceDetection.FaceDetection({
+        locateFile: (file) =>
+          `https://cdn.jsdelivr.net/npm/@mediapipe/face_detection/${file}`,
+      }),
+      camera: ({ mediaSrc, onFrame, width, height }) =>
+        new Camera(mediaSrc, {
+          onFrame,
+          width,
+          height,
+        }),
+    }
+  );
 
-	//Image Capturing--------->>>>>>>>
-	useEffect(() => {
-		if (seconds > 0) {
-			setTimeout(() => {
-				setSeconds(seconds - 1);
-			}, 1000);
-		}
-	}, [seconds]);
+  //Image Capturing--------->>>>>>>>
+  useEffect(() => {
+    if (seconds > 0) {
+      setTimeout(() => {
+        setSeconds(seconds - 1);
+      }, 1000);
+    }
+  }, [seconds]);
 
-	// const biometicBlob = async (base64) => {
-	// 	await fetch(base64).then(async (res) => setBioImage(await res.blob()));
-	// };
+  // const biometicBlob = async (base64) => {
+  // 	await fetch(base64).then(async (res) => setBioImage(await res.blob()));
+  // };
 
-	const clickImage = () => {
-		setTimeout(async () => {
-			if (facesDetected != 1) {
-				setWarning(true);
-			} else {
-				picture = webcamRef?.current?.getScreenshot({
-					width: 400,
-					height: 400,
-				});
+  const clickImage = () => {
+    setTimeout(async () => {
+      if (facesDetected != 1) {
+        setWarning(true);
+      } else {
+        picture = webcamRef?.current?.getScreenshot({
+          width: 400,
+          height: 400,
+        });
 
-				console.log("pic clicked");
-				setImage(picture);
-				localStorage.setItem("capImg", JSON.stringify(picture));
-			}
-		}, 2000);
-	};
+        console.log("pic clicked");
+        setImage(picture);
+        localStorage.setItem("capImg", JSON.stringify(picture));
+      }
+    }, 5000);
+  };
 
-	console.log("image section renderd");
+  console.log("image section renderd");
 
-	// if (isLoading) {
-	// 	return null;
-	// }
+  // if (isLoading) {
+  // 	return null;
+  // }
 
-	return (
-		<div>
-			<div>
-				{image ? (
-					<>
-						{/* {window.location.reload(false)} */}
+  return (
+    <div>
+      <div>
+        {image ? (
+          <>
+            {/* {window.location.reload(false)} */}
 
-						<div className="row justify-content-center g-3 m-3">
-							<div className="col-md-9">
-								<img src={image} className="biometric-image" />
-							</div>
+            <div className="row justify-content-center g-3 m-3">
+              <div className="col-md-9">
+                <img src={image} className="biometric-image" />
+              </div>
 
-							<div className="alert alert-success col-md-9" role="alert">
-								Image captured succesfully!!!
-							</div>
+              <div className="alert alert-success col-md-9" role="alert">
+                Image captured succesfully!!!
+              </div>
 
-							<div className="col-5">
-								<button
-									className="btn btn-primary w-100 py-2 btn-primary"
-									onClick={() => {
-										setFlag(!flag);
-										setSeconds(10);
-										setImage(null);
-										setWarning(false);
-										clickImage();
-										window.location.reload(false);
-									}}>
-									Retake
-								</button>
-							</div>
+              <div className="col-5">
+                <button
+                  className="btn btn-primary w-100 py-2 btn-primary"
+                  onClick={() => {
+                    setFlag(!flag);
+                    setSeconds(5);
+                    setImage(null);
+                    setWarning(false);
+                    clickImage();
+                    window.location.reload(false);
+                  }}
+                >
+                  Retake
+                </button>
+              </div>
 
-							<div className="col-5">
-								<button
-									className="btn btn-primary w-100 py-2 btn-primary"
-									onClick={() => {
-										window.location.href = "/register4";
-										// navigate("/register4");
-									}}>
-									Next Page
-								</button>
-							</div>
-						</div>
-					</>
-				) : (
-					<div className="row justify-content-center">
-						{boundingBox.map((box, index) => (
-							<div
-								key={`${index + 1}`}
-								style={{
-									border: "2px solid green",
-									position: "absolute",
-									top: `${box.yCenter * 80}%`,
-									left: `${box.xCenter * 95}%`,
-									width: `${box.width * 100}%`,
-									height: `${box.height * 100}%`,
-									zIndex: 1,
-								}}
-							/>
-						))}
-						<Webcam
-							style={{
-								width: "600px",
-								height: "500px",
-								position: "relative",
-								//marginBottom: "20px",
-								marginLeft: "auto",
-								marginRight: "auto",
-							}}
-							ref={webcamRef}
-						/>
+              <div className="col-5">
+                <button
+                  className="btn btn-primary w-100 py-2 btn-primary"
+                  onClick={() => {
+                    window.location.href = "/register4";
+                    // navigate("/register4");
+                  }}
+                >
+                  Next Page
+                </button>
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="row justify-content-center">
+            {boundingBox.map((box, index) => (
+              <div
+                key={`${index + 1}`}
+                style={{
+                  border: "2px solid green",
+                  position: "absolute",
+                  top: `${box.yCenter * 70}%`,
+                  left: `${box.xCenter * 95}%`,
+                  width: `${box.width * 100}%`,
+                  height: `${box.height * 90}%`,
+                  zIndex: 1,
+                }}
+              />
+            ))}
+            <Webcam
+              style={{
+                width: "600px",
+                height: "500px",
+                position: "relative",
+                //marginBottom: "20px",
+                marginLeft: "auto",
+                marginRight: "auto",
+              }}
+              ref={webcamRef}
+            />
 
-						{warning ? (
-							<div className="row justify-content-center">
-								<div className="form-floating col-md-8">
-									<div className="alert alert-danger" role="alert">
-										{/* <svg className="bi" role="img" aria-label="Danger:">
+            {warning ? (
+              <div className="row justify-content-center">
+                <div className="form-floating col-md-8">
+                  <div className="alert alert-danger" role="alert">
+                    {/* <svg className="bi" role="img" aria-label="Danger:">
 								<use xlinkHref="#exclamation-triangle-fill" />
 							</svg> */}
-										<div className="mt-0">
-											Image can only be clicked when there is only one person in
-											the frame!! Curently, the number of faces recognised are{" "}
-											{facesDetected}
-										</div>
-									</div>
-								</div>
+                    <div className="mt-0">
+                      Image can only be clicked when there is only one person in
+                      the frame!! Curently, the number of faces recognised are{" "}
+                      {facesDetected}
+                    </div>
+                  </div>
+                </div>
 
-								<div className="col-6">
-									<button
-										className="btn btn-primary w-100 py-2 btn-primary"
-										onClick={() => {
-											setFlag(!flag);
-											setSeconds(10);
-											setImage(null);
-											setWarning(false);
-											clickImage();
-										}}>
-										Retake
-									</button>
-								</div>
-							</div>
-						) : (
-							<div className="col-6">
-								<button
-									// type="submit"
-									className="btn btn-primary w-100 py-3 btn-primary"
-									onClick={() => {
-										setSeconds(2);
-										clickImage();
-									}}>
-									Take Picture
-								</button>
-							</div>
-						)}
-					</div>
-				)}
+                <div className="col-6">
+                  <button
+                    className="btn btn-primary w-100 py-2 btn-primary"
+                    onClick={() => {
+                      setFlag(!flag);
+                      setSeconds(5);
+                      setImage(null);
+                      setWarning(false);
+                      clickImage();
+                    }}
+                  >
+                    Retake
+                  </button>
+                </div>
+              </div>
+            ) : (
+              <div className="col-6">
+                <button
+                  // type="submit"
+                  className="btn btn-primary w-100 py-3 btn-primary"
+                  onClick={() => {
+                    setSeconds(5);
+                    clickImage();
+                  }}
+                >
+                  Take Picture
+                </button>
+              </div>
+            )}
+          </div>
+        )}
 
-				{seconds > 0 ? <div className="seconds">{seconds}</div> : null}
-			</div>
-		</div>
-	);
+        {seconds > 0 ? <div className="seconds">{seconds}</div> : null}
+      </div>
+    </div>
+  );
 }
 
 export default CaptureImage;
--- src/hooks/useHandleValidation.jsx
+++ src/hooks/useHandleValidation.jsx
@@ -5,66 +5,66 @@ import { useContext, useState } from "react";
 import { UserContext } from "../context/UserContext";
 
 export const useHandleValidation = (
-	initialValues,
-	validationSchema,
-	url,
-	api,
-	token,
-	signUp
+  initialValues,
+  validationSchema,
+  url,
+  api,
+  token,
+  signUp
 ) => {
-	const { setUser, setToken } = useContext(UserContext);
+  const { setUser, setToken } = useContext(UserContext);
 
-	const [validUser, setValidUser] = useState(true);
+  const [validUser, setValidUser] = useState(true);
 
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	const { values, errors, touched, handleBlur, handleChange, handleSubmit } =
-		useFormik({
-			initialValues,
-			validationSchema,
-			// onSubmit: (values) => {
-			//   console.log(values);
-			// },
+  const { values, errors, touched, handleBlur, handleChange, handleSubmit } =
+    useFormik({
+      initialValues,
+      validationSchema,
+      // onSubmit: (values) => {
+      //   console.log(values);
+      // },
 
-			onSubmit: async (values) => {
-				// console.log(values);
+      onSubmit: async (values) => {
+        // console.log(values);
 
-				const config = {
-					method: "post",
-					url: api,
-					headers: { "Content-Type": "application/json", authorization: token },
-					data: values,
-				};
+        const config = {
+          method: "post",
+          url: api,
+          headers: { "Content-Type": "application/json", authorization: token },
+          data: values,
+        };
 
-				let response = await ApiCall(config);
+        let response = await ApiCall(config);
 
-				if (response.status === 201) {
-					// console.log(response.data);
-					// console.log(signUp);
-					if (signUp) {
-						// console.log(response.data.data.result.status);
+        if (response.status === 201) {
+          // console.log(response.data);
+          // console.log(signUp);
+          if (signUp) {
+            // console.log(response.data.data.result.status);
 
-						if (response?.data?.data?.result?.status == 203) {
-							// setValidUser(false);
-							errors.email = "Email already exists! Please login to continue!";
-						} else {
-							setUser(response?.data?.data?.result);
-							// console.log(response?.data?.data?.result);
-							localStorage.setItem(
-								"localUser",
-								JSON.stringify(response?.data?.data?.result)
-							);
-							setToken(response?.data?.data?.auth);
-							navigate(url);
-						}
-					}
-					// console.log("validuser before navigate---", validUser);
-					else navigate(url);
-				} else {
-					alert("Something went wrong!!!");
-				}
-			},
-		});
+            if (response?.data?.data?.result?.status == 203) {
+              // setValidUser(false);
+              errors.email = "Email already exists! Please login to continue!";
+            } else {
+              setUser(response?.data?.data?.result);
+              // console.log(response?.data?.data?.result);
+              localStorage.setItem(
+                "localUser",
+                JSON.stringify(response?.data?.data)
+              );
+              setToken(response?.data?.data?.auth);
+              navigate(url);
+            }
+          }
+          // console.log("validuser before navigate---", validUser);
+          else navigate(url);
+        } else {
+          alert("Something went wrong!!!");
+        }
+      },
+    });
 
-	return { values, errors, touched, handleBlur, handleChange, handleSubmit };
+  return { values, errors, touched, handleBlur, handleChange, handleSubmit };
 };
