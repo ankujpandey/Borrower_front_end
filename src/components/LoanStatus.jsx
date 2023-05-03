8fe52f2e06f332c2fc87bd4b00c7fb000a04c2c0
ankujpandey <pandeyankuj@gmail.com>
2023-05-03 18:29:47 +0530

    added loan status page

--- src/components/LoanStatus.jsx
+++ src/components/LoanStatus.jsx
@@ -1,95 +1,50 @@
-import React, { useEffect, useState } from "react";
-import RegisteredDetails from "./RegisteredDetails";
+import React from "react";
 
-function LoanStatus({ loanStatus }) {
-  console.log(loanStatus);
-  const [profilePage, setProfilePage] = useState(true);
-  const [statusPage, setStatusPage] = useState(false);
-  const [emiPage, setEmiPage] = useState(false);
-  const [passBookPage, setPassBookPage] = useState(false);
+function LoanStatus(props) {
+	console.log("Loan Status------>>>>", props);
+	return (
+		<div className="container px-lg-5">
+			<div className="row m-5">
+				<div className="col-12">
+					<h4>Loan Status</h4>
+					<hr className="mt-2 mb-1" />
+					<div>
+						<p className="calculator-msg">
+							You're Application is under process.
+						</p>
 
-  // useEffect(() => {}, [profilePage]);
+						<table className="table-without-line">
+							<tbody>
+								<tr>
+									<th>
+										<p>Amount Requested: </p>
+									</th>
+								</tr>
+								<tr>
+									<th>
+										<p>Duration: </p>
+									</th>
+								</tr>
+								<tr>
+									<th>
+										<p>Rate of Interest Expected: </p>
+									</th>
+								</tr>
+							</tbody>
+						</table>
 
-  return (
-    <div className="container px-lg-7">
-      <div className="row justify-content-center">
-        <div className="col-lg-10">
-          <div
-            className="card shadow mb-5 bg-body-tertiary rounded wow fadeInUp"
-            data-wow-delay="0.3s"
-          >
-            <div className="d-flex">
-              <div className="col-2 round-nav ps-3 pt-4 pb-4 ">
-                <nav className="nav flex-column">
-                  <a
-                    className={`nav-link ${profilePage ? "active" : ""}`}
-                    aria-current="page"
-                    onClick={() => {
-                      setProfilePage(true);
-                      setStatusPage(false);
-                      setEmiPage(false);
-                      setPassBookPage(false);
-                    }}
-                    href="#"
-                  >
-                    <span className={`${profilePage ? "enable" : ""}`}>
-                      Profile
-                    </span>
-                  </a>
-                  <a
-                    className={`nav-link ${statusPage ? "active" : ""}`}
-                    onClick={() => {
-                      setProfilePage(false);
-                      setStatusPage(true);
-                      setEmiPage(false);
-                      setPassBookPage(false);
-                    }}
-                    href="#"
-                  >
-                    <span className={`${statusPage ? "enable" : ""}`}>
-                      Loan Status
-                    </span>
-                  </a>
-                  <a
-                    className={`nav-link ${emiPage ? "active" : ""}`}
-                    onClick={() => {
-                      setProfilePage(false);
-                      setStatusPage(false);
-                      setEmiPage(true);
-                      setPassBookPage(false);
-                    }}
-                    href="#"
-                  >
-                    <span className={`${emiPage ? "enable" : ""}`}>
-                      EMI Details
-                    </span>
-                  </a>
-                  <a
-                    className={`nav-link ${passBookPage ? "active" : ""}`}
-                    onClick={() => {
-                      setProfilePage(false);
-                      setStatusPage(false);
-                      setEmiPage(false);
-                      setPassBookPage(true);
-                    }}
-                  >
-                    <span className={`${passBookPage ? "enable" : ""}`}>
-                      Passbook
-                    </span>
-                  </a>
-                </nav>
-              </div>
-              {profilePage ? (
-                <div className="col-10">
-                  <RegisteredDetails loanStatus={loanStatus} />
-                </div>
-              ) : null}
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+						<p>Amount Approved: </p>
+						<p>Duration Approved: </p>
+						<p>Minimum interest rate Approved: </p>
+						<p>Maximum interest rate Approved: </p>
+						<p>Amount Funded: </p>
+						<p>Remaining Amount: </p>
+						<p>Amount Disbursed: </p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default LoanStatus;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,93 +2,91 @@ import React, { useContext, useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import { UserContext } from "../context/UserContext";
 import RegisteredDetails from "../components/RegisteredDetails";
-import LoanStatus from "../components/LoanStatus";
+import UserDashboard from "../components/UserDashboard";
 import { ApiCall } from "../functions/ApiCall";
 
 function Dashboard(props) {
-  const { user, token } = useContext(UserContext);
-  const [loanStatus, setLoanStatus] = useState(null);
-  const uid = user.userName.uid;
-  // console.log("uid", uid);
+	const { user, token } = useContext(UserContext);
+	const [loanStatus, setLoanStatus] = useState(null);
+	const uid = user.userName.uid;
+	// console.log("uid", uid);
 
-  const updatedUser = JSON.parse(localStorage.getItem("userPersonalDetails"));
+	const updatedUser = JSON.parse(localStorage.getItem("userPersonalDetails"));
 
-  useEffect(() => {
-    fetchLoanDetails(uid);
-  }, []);
+	useEffect(() => {
+		fetchLoanDetails(uid);
+	}, []);
 
-  const fetchLoanDetails = async (uid) => {
-    const config = {
-      method: "get",
-      url: `http://localhost:4000/api/v1/getLoanStatus/${uid}`,
-      headers: { "Content-Type": "application/json", authorization: token },
-    };
+	const fetchLoanDetails = async (uid) => {
+		const config = {
+			method: "get",
+			url: `http://localhost:4000/api/v1/getLoanStatus/${uid}`,
+			headers: { "Content-Type": "application/json", authorization: token },
+		};
 
-    let response = await ApiCall(config);
-    if (response?.status == 201) {
-      console.log(response?.data?.data);
-      setLoanStatus(response.data.data.cndtn_code);
-    }
-  };
+		let response = await ApiCall(config);
+		if (response?.status == 201) {
+			console.log(response?.data?.data);
+			setLoanStatus(response.data.data.cndtn_code);
+		}
+	};
 
-  return (
-    <div>
-      <div className="py-5 bg-primary hero-header mb-3">
-        <div className="container py-3 px-5">
-          <div className="row mt-5">
-            <div className="col-12 text-center">
-              <div data-wow-delay="0.1s">
-                <h1 className="text-white animated zoomIn mt-5">
-                  Welcome {user?.userName?.firstName} !
-                </h1>
-              </div>
-              <hr
-                className="bg-white mx-auto mt-0 mb-5"
-                style={{ width: 90 }}
-              />
-              <nav aria-label="breadcrumb"></nav>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div
-        className="section-title position-relative text-center mb-5 pb-2 wow fadeInUp"
-        data-wow-delay="0.1s"
-      >
-        <h6 className="position-relative d-inline text-primary ps-4">
-          Dash Board
-        </h6>
-        {loanStatus ? null : user?.userName?.contact ? (
-          <h2 className="mt-2">
-            Congratulations! Your registration is completed.
-          </h2>
-        ) : (
-          <h2 className="mt-2">Press Button To Continue</h2>
-        )}
-      </div>
+	return (
+		<div>
+			<div className="py-5 bg-primary hero-header mb-3">
+				<div className="container py-3 px-5">
+					<div className="row mt-5">
+						<div className="col-12 text-center">
+							<div data-wow-delay="0.1s">
+								<h1 className="text-white animated zoomIn mt-5">
+									Welcome {user?.userName?.firstName} !
+								</h1>
+							</div>
+							<hr
+								className="bg-white mx-auto mt-0 mb-5"
+								style={{ width: 90 }}
+							/>
+							<nav aria-label="breadcrumb"></nav>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div
+				className="section-title position-relative text-center mb-5 pb-2 wow fadeInUp"
+				data-wow-delay="0.1s">
+				<h6 className="position-relative d-inline text-primary ps-4">
+					Dash Board
+				</h6>
+				{loanStatus ? null : user?.userName?.contact ? (
+					<h2 className="mt-2">
+						Congratulations! Your registration is completed.
+					</h2>
+				) : (
+					<h2 className="mt-2">Press Button To Continue</h2>
+				)}
+			</div>
 
-      {loanStatus ? (
-        <LoanStatus loanStatus={loanStatus} />
-      ) : user?.userName?.contact || updatedUser?.contact ? (
-        <RegisteredDetails />
-      ) : (
-        <div className="row d-flex justify-content-center">
-          <div className="col-lg-5 d-flex justify-content-center">
-            <div className="col-10 card wow fadeInUp" data-wow-delay="0.3s">
-              <NavLink to="/personal-details">
-                <button
-                  type="submit"
-                  className="btn btn-primary w-100 py-3 btn-primary"
-                >
-                  Register
-                </button>
-              </NavLink>
-            </div>
-          </div>
-        </div>
-      )}
-    </div>
-  );
+			{loanStatus ? (
+				<UserDashboard loanStatus={loanStatus} />
+			) : user?.userName?.contact || updatedUser?.contact ? (
+				<RegisteredDetails />
+			) : (
+				<div className="row d-flex justify-content-center">
+					<div className="col-lg-5 d-flex justify-content-center">
+						<div className="col-10 card wow fadeInUp" data-wow-delay="0.3s">
+							<NavLink to="/personal-details">
+								<button
+									type="submit"
+									className="btn btn-primary w-100 py-3 btn-primary">
+									Register
+								</button>
+							</NavLink>
+						</div>
+					</div>
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default Dashboard;
